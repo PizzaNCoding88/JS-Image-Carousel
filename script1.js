025de53e6e1451ed475f33ccaf91ea8ce7256d5b
PizzaNCoding88 <110925628+PizzaNCoding88@users.noreply.github.com>
2023-03-01 11:12:22 +0000

    pagination completed

--- script1.js
+++ script1.js
@@ -8,37 +8,52 @@ nextBtn.addEventListener("click", () => {
         slideOnOff(currentSlide);
         currentSlide = document.querySelector(".first");
         slideOnOff(currentSlide);
+
+        dotsOnOff(currentDot);
+        currentDot = document.querySelector(".firstdot");
+        dotsOnOff(currentDot);
     } else{
         const currentSlide = document.querySelector(".active");
         const nextSlide = currentSlide.nextElementSibling;
+        const currentDot = document.querySelector(".on");
+        const nextDot = currentDot.nextElementSibling;
         slideOnOff(currentSlide);
         slideOnOff(nextSlide);
+        dotsOnOff(currentDot);
+        dotsOnOff(nextDot);
     }
 });
 
+
 previousBtn.addEventListener("click", ()=>{
     let currentSlide = document.querySelector(".active");
+    let currentDot = document.querySelector(".on");
     if(currentSlide.classList.contains("first")){
         slideOnOff(currentSlide);
         currentSlide = document.querySelector(".last");
         slideOnOff(currentSlide);
+        dotsOnOff(currentDot);
+        currentDot = document.querySelector(".lastdot");
+        dotsOnOff(currentDot);
     } else{
         const currentSlide = document.querySelector(".active");
         const previousSlide = currentSlide.previousElementSibling;
+        const currentDot = document.querySelector(".on");
+        const previousDot = currentDot.previousElementSibling;
         slideOnOff(currentSlide);
         slideOnOff(previousSlide);
+        dotsOnOff(currentDot);
+        dotsOnOff(previousDot);
     }
 });
 
 
-
+function dotsOnOff(dot){
+    dot.classList.toggle("on");
+    dot.classList.toggle("off");
+};
 
 function slideOnOff(slide){
     slide.classList.toggle("active");
     slide.classList.toggle("disabled");
 };
-
-function dotsOnOff(dot){
-    dot.classList.toggle("active");
-    dot.classList.toggle("disabled");
-}
